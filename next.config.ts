6c0a969320cca0b55701b5ffd8e34add1ba1a40e
Shubharambh Hotels <shubharambh.banquet2026@gmail.com>
2026-09-03 10:38:54 +0530

    Fix images for production

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "img.youtube.com" },
     ],
-    formats: ["image/avif", "image/webp"],
   },
 };
 
